d4a3310300de07a5318f7b4a308fdfc7951fed96
wurbuzz <mitchell.burt@hotmail.com>
2024-07-04 18:12:58 -0400

    Update pokedex.c

--- src/pokedex.c
+++ src/pokedex.c
@@ -7720,13 +7720,15 @@ static void handleTargetSpeciesPrint(u8 taskId, u16 targetSpecies, u8 base_x, u8
     {
         LoadMonIconPalette(targetSpecies); //Loads pallete for current mon
         #ifndef POKEMON_EXPANSION
-            if (seen || !HGSS_HIDE_UNSEEN_EVOLUTION_NAMES)
-                gTasks[taskId].data[4+base_i] = CreateMonIcon(targetSpecies, SpriteCB_MonIcon, 50 + 32*base_i, 31, 4, 0, formId); //Create pokemon sprite
+            gTasks[taskId].data[4+base_i] = CreateMonIcon(targetSpecies, SpriteCB_MonIcon, 50 + 32*base_i, 31, 4, 0, formId); //Create pokemon sprite
         #endif
         #ifdef POKEMON_EXPANSION
             gTasks[taskId].data[4+base_i] = CreateMonIcon(targetSpecies, SpriteCB_MonIcon, 50 + 32*base_i, 31, 4, 0, formId); //Create pokemon sprite
         #endif
-        gSprites[gTasks[taskId].data[4+base_i]].oam.priority = 0;
+        if (seen || !HGSS_HIDE_UNSEEN_EVOLUTION_NAMES){
+            gSprites[gTasks[taskId].data[4+base_i]].oam.priority = 0;
+        }
+
     }
 }
 
