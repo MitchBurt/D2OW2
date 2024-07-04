c375997fccbe2c22f6a5eeaea064e1e29372e82b
wurbuzz <mitchell.burt@hotmail.com>
2024-07-04 13:37:45 -0400

    Update pokedex.c

--- src/pokedex.c
+++ src/pokedex.c
@@ -349,7 +349,7 @@ static void CreateTypeIconSprites(void);
 #define SCROLLING_MON_X 146
 #define HGSS_DECAPPED 0 //0 false, 1 true
 #define HGSS_DARK_MODE 1 //0 false, 1 true
-#define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES 0 //0 false, 1 true
+#define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES 1 //0 false, 1 true
 static void LoadTilesetTilemapHGSS(u8 page);
 static void Task_HandleStatsScreenInput(u8 taskId);
 static void Task_LoadStatsScreen(u8 taskId);
