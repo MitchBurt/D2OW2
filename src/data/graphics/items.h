0a3718c61f4dd4b97ec17b3e742ff55a44ce2a41
wurbuzz <mitchell.burt@hotmail.com>
2024-07-30 10:56:21 -0400

    sceptilite

--- src/data/graphics/items.h
+++ src/data/graphics/items.h
@@ -1367,7 +1367,7 @@ const u32 gItemIcon_Baitankite[] = INCBIN_U32("graphics/items/icons/baitankite.4
 const u32 gItemIconPalette_Baitankite[] = INCBIN_U32("graphics/items/icon_palettes/baitankite.gbapal.lz");
 
 const u32 gItemIcon_Incredalenite[] = INCBIN_U32("graphics/items/icons/incredalenite.4bpp.lz");
-const u32 gItemIconPalette_Sceptilite[] = INCBIN_U32("graphics/items/icon_palettes/sceptilite.gbapal.lz");
+const u32 gItemIconPalette_Incredalenite[] = INCBIN_U32("graphics/items/icon_palettes/incredalenite.gbapal.lz");
 
 const u32 gItemIcon_Sablenite[] = INCBIN_U32("graphics/items/icons/sablenite.4bpp.lz");
 const u32 gItemIconPalette_Sablenite[] = INCBIN_U32("graphics/items/icon_palettes/sablenite.gbapal.lz");
