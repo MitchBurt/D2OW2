441cdb1a4a2b4fb8075f079c744b57d68b2a6950
wurbuzz <mitchell.burt@hotmail.com>
2024-11-15 15:35:50 -0500

    gNewGamePCItems
    Removed Pants from First Stage Pkm

--- src/new_game.c
+++ src/new_game.c
@@ -457,14 +457,15 @@ void NewGameInitData(void)
         
         SetCoins(0);
         ResetPokemonStorageSystem();
+        for (i = 0; i < PC_ITEMS_COUNT; i++)
+        {
+            gNewGamePCItems[i] = gSaveBlock1Ptr->pcItems[i];
+        }
+        
         NewGameInitPCItems();
         memset(&gSaveBlock2Ptr->unlockedQuests, 0, sizeof(gSaveBlock2Ptr->unlockedQuests));
         memset(&gSaveBlock2Ptr->completedQuests, 0, sizeof(gSaveBlock2Ptr->completedQuests));
     
-    for (i = 0; i < PC_ITEMS_COUNT; i++)
-        {
-            pcItemsplus[i] = gSaveBlock1Ptr->pcItems[i];
-        }
     }
     else{
         SetMoney(&gSaveBlock1Ptr->money, money);
@@ -491,7 +492,10 @@ void NewGameInitData(void)
         }
 
         //PC Items
-
+        for (i = 0; i < PC_ITEMS_COUNT; i++)
+        {
+            pcItemsplus[i] = gSaveBlock1Ptr->pcItems[i];
+        }
         //TMs
         for (i = 0; i < NUM_TMS; i++)
         {
--- src/player_pc.c
+++ src/player_pc.c
@@ -169,11 +169,8 @@ static const struct MenuAction gPCText_ItemPCOptionsText[] =
     { gText_Cancel, ItemStorage_Exit }
 };
 
-static const struct ItemSlot gNewGamePCItems[] =
-{
-    { ITEM_POTION, 1 },
-    { ITEM_NONE, 0 }
-};
+struct ItemSlot gNewGamePCItems[];
+
 
 const struct MenuAction gMailboxMailOptions[] =
 {
@@ -308,8 +305,8 @@ void NewGameInitPCItems(void)
 {
     u8 i;
     // because Game Freak don't know how to use a struct or a 2d array
-   //for(i = 0, ClearItemSlots(gSaveBlock1Ptr->pcItems, ARRAY_COUNT(gSaveBlock1Ptr->pcItems)); NEW_GAME_PC_ITEMS(i, PC_ITEM_ID) && NEW_GAME_PC_ITEMS(i, PC_QUANTITY) &&
-       // AddPCItem(NEW_GAME_PC_ITEMS(i, PC_ITEM_ID), NEW_GAME_PC_ITEMS(i, PC_QUANTITY)) == TRUE; i++);
+    for(i = 0, ClearItemSlots(gSaveBlock1Ptr->pcItems, ARRAY_COUNT(gSaveBlock1Ptr->pcItems)); NEW_GAME_PC_ITEMS(i, PC_ITEM_ID) && NEW_GAME_PC_ITEMS(i, PC_QUANTITY) &&
+        AddPCItem(NEW_GAME_PC_ITEMS(i, PC_ITEM_ID), NEW_GAME_PC_ITEMS(i, PC_QUANTITY)) == TRUE; i++);
 }
 
 void BedroomPC(void)
