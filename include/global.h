58dd1bc8236f9e68857aca89446761a2121aa29b
wurbuzz <mitchell.burt@hotmail.com>
2024-11-29 15:08:15 -0500

    Coping

--- include/global.h
+++ include/global.h
@@ -1015,8 +1015,8 @@ struct SaveBlock1
               struct ItemSlot bagPocket_PowerUp[BAG_POWERUP_COUNT];
               struct ItemSlot bagPocket_TypeItems[BAG_TYPEITEMS_COUNT];
               struct ItemSlot bagPocket_MegaStones[BAG_MEGASTONES_COUNT];
-    /*0x848*/ struct Pokeblock pokeblocks[POKEBLOCKS_COUNT];
-    /*0x988*/ struct ItemSlot bagPocket_Charms[BAG_CHARMS_COUNT];
+    /*0x848*/ struct ItemSlot bagPocket_Charms[BAG_CHARMS_COUNT];
+    /*0x988*/ struct Pokeblock pokeblocks[POKEBLOCKS_COUNT];
     /*0x9BC*/ u16 berryBlenderRecords[3];
     /*0x9C2*/ u8 field_9C2[6]; //Filler
     /*0x9C8*/ u16 trainerRematchStepCounter;
@@ -1102,9 +1102,6 @@ struct TradeRoomPlayer
     struct MapPosition pos;
     u16 field_C;
 };
-
-//#define MAX_ACTIVE_CHARMS 10
-//extern u16 gActiveCharms[MAX_ACTIVE_CHARMS]; // Declare the array globally
 
 
 #endif // GUARD_GLOBAL_H
