ef99d3dbf2479e8d030e40f736bd1a69a3b1954c
wurbuzz <mitchell.burt@hotmail.com>
2024-12-09 17:18:49 -0500

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3661,6 +3661,7 @@ static void Cmd_tryfaintmon(void)
                         bool8 dead = TRUE;
                         SetMonData(mon, MON_DATA_DEAD, &dead);
                     }
+                }
 
 
                 gHitMarker |= HITMARKER_x400000;
